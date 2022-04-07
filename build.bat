786ba065261aa9c678367bef19fe45274041c676
Matthew Sitton <matthewsitton@gmail.com>
2022-04-06 23:57:05 -0500

    Add build.bat for windows

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet publish /p:DebugType=None -c Release -r win-x64 --self-contained --output .\bin\build\win-x64
+dotnet publish /p:DebugType=None -c Release -r win-x86 --self-contained --output .\bin\build\win-x86
+pause
